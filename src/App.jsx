ffdc3198956de476cb7a0dc7e866aad16397bc7f
harsh shah <harshshah301204@gmail.com>
2025-12-04 20:18:49 +0530

    custome cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/home";
+import { SmoothCursor } from "./components/ui/smooth-cursor";
 
 function App() {
 
 
   return (
     <>
+      <SmoothCursor />
       <Home />
     </>
   );
